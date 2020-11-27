de10496e19643ce5ea16502dd3c4944c3798aa48
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-11-27 17:49:39 +0000

    Fix macro exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /**
 The expansion of this macro generates a typestated `struct` with a `__state` field.
 */
-macro_rules! gen_struct {
+macro_rules! __gen_struct {
     ($vis:vis $struct_name:ident <$state_name:ident> {$($field:ident:$field_type:ty),*}) => {
         $vis struct $struct_name<$state_name> {
             __state: std::marker::PhantomData<$state_name>,
@@ -22,7 +22,7 @@ macro_rules! gen_struct {
 /**
 Generates the empty `struct`s which represent state on the typesystem.
 */
-macro_rules! gen_typestate_structs {
+macro_rules! __gen_typestate_structs {
     ($vis:vis $($typestate:ident),+) => {
         $($vis struct $typestate;)+
     }
@@ -31,7 +31,7 @@ macro_rules! gen_typestate_structs {
 /**
 Generates the sealed trait implementation.
 */
-macro_rules! gen_sealed {
+macro_rules! __gen_sealed {
     ($mod:ident::$trait:ident [$($typestate:ident),+]) => {
         mod $mod {
             pub trait $trait {}
@@ -39,23 +39,23 @@ macro_rules! gen_sealed {
         }
     };
     ($trait:ident [$($typestate:ident),+]) => {
-        gen_sealed!(sealed::$trait [$($typestate),+]);
+        __gen_sealed!(sealed::$trait [$($typestate),+]);
     };
     ([$($typestate:ident),+]) => {
-        gen_sealed!(sealed::Sealed [$($typestate),+]);
+        __gen_sealed!(sealed::Sealed [$($typestate),+]);
     };
 }
 
 /**
 Generates the traits for the `limited` and `strict` typestate variants.
 */
-macro_rules! gen_state_trait {
+macro_rules! __gen_state_trait {
     ($vis:vis $trait_name:ident [$($typestate:ident),+]) => {
         $vis trait $trait_name {}
         $(impl $trait_name for $typestate {})+
     };
     (strict $vis:vis $trait_name:ident ($sealed_mod:ident::$sealed_trait:ident) [$($typestate:ident),+]) => {
-        gen_sealed!($sealed_mod::$sealed_trait [$($typestate),+]);
+        __gen_sealed!($sealed_mod::$sealed_trait [$($typestate),+]);
         $vis trait $trait_name: $sealed_mod::$sealed_trait {}
         $(impl $trait_name for $typestate {})+
     };
@@ -126,11 +126,11 @@ typestate!(
 );
 ```
 */
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! typestate {
     ($vis:vis $struct_name:ident <$state_name:ident> [$($typestate:ident),+] {$($field:ident:$field_ty:ty),*}) => {
-        gen_typestate_structs!($vis $($typestate),+);
-        gen_struct!($vis $struct_name<$state_name> {$($field:$field_ty),*});
+        __gen_typestate_structs!($vis $($typestate),+);
+        __gen_struct!($vis $struct_name<$state_name> {$($field:$field_ty),*});
     };
     (limited
         $vis:vis
@@ -139,9 +139,9 @@ macro_rules! typestate {
         [$($typestate:ident),+]
         {$($field:ident:$field_ty:ty),*}
     ) => {
-        gen_typestate_structs!($vis $($typestate),+);
-        gen_state_trait!($vis $state_trait [$($typestate),+]);
-        gen_struct!($vis $struct_name<$state_name:$state_trait> {$($field:$field_ty),*});
+        __gen_typestate_structs!($vis $($typestate),+);
+        __gen_state_trait!($vis $state_trait [$($typestate),+]);
+        __gen_struct!($vis $struct_name<$state_name:$state_trait> {$($field:$field_ty),*});
     };
     (limited
         $vis:vis
@@ -180,9 +180,9 @@ macro_rules! typestate {
         [$($typestate:ident),+]
         {$($field:ident:$field_ty:ty),*}
     ) => {
-        gen_typestate_structs!($vis $($typestate),+);
-        gen_state_trait!(strict $vis $state_trait ($sealed_mod::$sealed_trait) [$($typestate),+]);
-        gen_struct!($vis $struct_name<$state_name:$state_trait> {$($field:$field_ty),*});
+        __gen_typestate_structs!($vis $($typestate),+);
+        __gen_state_trait!(strict $vis $state_trait ($sealed_mod::$sealed_trait) [$($typestate),+]);
+        __gen_struct!($vis $struct_name<$state_name:$state_trait> {$($field:$field_ty),*});
     };
     (strict
         $vis:vis
