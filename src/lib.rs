7d89db4d7631fbedef1a7326c854d941f0fe414c
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-02-22 12:14:37 +0000

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use syn::spanned::Spanned;
 
 use {
     proc_macro2::TokenStream,
-    syn::{parse_macro_input, Error, Item, Result},
+    syn::{parse_macro_input, Error, ItemMod, Result},
 };
 
 #[proc_macro_attribute]
@@ -11,18 +11,20 @@ pub fn typestate(
     _: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let module = parse_macro_input!(input as Item);
+    let module = parse_macro_input!(input as ItemMod);
     println!("{:#?}", module);
     match parse_module_item(module) {
         Ok(tt) => tt,
         Err(error) => error.to_compile_error(),
     }
     .into()
+    // TokenStream::new().into()
 }
 
-fn parse_module_item(item: Item) -> Result<TokenStream> {
-    match item {
-        Item::Mod(module) => Ok(module.to_token_stream()),
-        _ => Err(Error::new(item.span(), "expected mod")),
+fn parse_module_item(item_mod: ItemMod) -> Result<TokenStream> {
+    if let Some((_, module_items)) = item_mod.content {
+        Ok(module_items.iter().map(|it| it.to_token_stream()).collect())
+    } else {
+        Err(Error::new(item_mod.span(), "module cannot be empty"))
     }
 }
