46f5dea72ce799d646d97d814b74d05a753e46a3
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-03-16 22:54:23 +0000

    Implement checks for misused inner attrs

--- typestate-macro/src/lib.rs
+++ typestate-macro/src/lib.rs
@@ -1,38 +1,47 @@
-use quote::{ToTokens, TokenStreamExt};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote, ToTokens, TokenStreamExt};
+use std::{collections::HashMap, iter::once};
 use syn::{
+    parse::Parser,
+    parse_macro_input, parse_quote,
+    spanned::Spanned,
     visit_mut::{self, VisitMut},
-    Attribute, Item, ItemEnum,
-};
-
-use {
-    proc_macro2::TokenStream,
-    syn::{parse_macro_input, spanned::Spanned, Error, ItemMod, ItemStruct, Result},
+    Attribute, Error, Field, Fields, Item, ItemEnum, ItemMod, ItemStruct, Result, TypeParam,
 };
 
 #[proc_macro_attribute]
 pub fn typestate(
-    _: proc_macro::TokenStream,
+    attrs: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let ref mut module = parse_macro_input!(input as Item);
-    // match parse_module_item(module) {
-    //     Ok(tt) => tt,
-    //     Err(error) => error.to_compile_error(),
-    // }
-    // .into()
+    // allow macro "misuses"
+    // e.g. #[typestate(non_existent)]
+    // this approach does not break future implementations
+    let _: syn::parse::Nothing = parse_macro_input!(attrs);
+    // parse the input as a mod
+    let mut module: ItemMod = parse_macro_input!(input);
+    // start visitor
     let mut visitor = StateMachineVisitor::new();
-    visitor.visit_item_mut(module);
+    visitor.visit_item_mod_mut(&mut module);
 
-    let errors = visitor.errors;
-    if !errors.is_empty() {
-        let mut err_out = TokenStream::new();
-        for error in errors {
-            err_out.append_all(error.into_compile_error())
-        }
-        return err_out.into()
+    let mut errors = visitor.errors;
+    if errors.is_empty() {
+        // if errors do not exist, return the token stream
+        let ret = module.into_token_stream();
+        // println!("{}", ret);
+        ret
+    } else {
+        // if errors exist, return all errors
+        let fst_err = errors.swap_remove(0);
+        errors
+            .into_iter()
+            .fold(fst_err, |mut all, curr| {
+                all.combine(curr);
+                all
+            })
+            .to_compile_error()
     }
-
-    module.to_token_stream().into()
+    .into()
 }
 
 struct StateMachineVisitor {
@@ -55,6 +64,23 @@ impl StateMachineVisitor {
             errors: Vec::new(),
         }
     }
+
+    fn push_multiple_attr_error(&mut self, attr: &Attribute) {
+        self.errors
+            .push(Error::new_spanned(attr, "multiple attributes are declared"));
+    }
+
+    fn push_multiple_decl_error(&mut self, attr: &Attribute, ident: &str) {
+        self.errors.push(Error::new_spanned(
+            attr,
+            format!("`{}` can only be used once", ident),
+        ));
+    }
+
+    fn push_multiple_automata_decl_error(&mut self, it: &ItemStruct) {
+        self.errors
+            .push(Error::new_spanned(it, "`automata` redefinition here"));
+    }
 }
 
 // /// This function finds valid attributes in the
@@ -70,25 +96,118 @@ impl visit_mut::VisitMut for StateMachineVisitor {
         let attributes = &it_struct.attrs;
         let mut remove_idx = vec![true; attributes.len()];
 
+        // filter out relevant attributes
+        let mut filtered = HashMap::new();
         for (idx, attr) in attributes.iter().enumerate() {
             if attr.path.is_ident(AUTOMATA_ATTR_IDENT) {
-                if self.main_struct.is_some() {
-                    // automata was previously defined
-                    self.errors.push(syn::Error::new_spanned(
-                        &it_struct,
-                        "`automata` redefinition is not allowed",
-                    ))
+                match filtered.get_mut(AUTOMATA_ATTR_IDENT) {
+                    None => {
+                        filtered.insert(AUTOMATA_ATTR_IDENT, vec![(idx, attr)]);
+                    }
+                    Some(v) => {
+                        v.push((idx, attr));
+                    }
                 }
-                self.main_struct = Some(it_struct.clone());
+                // mark for removal
                 remove_idx[idx] = false;
-            }
-            if attr.path.is_ident(STATE_ATTR_IDENT) {
+            } else if attr.path.is_ident(STATE_ATTR_IDENT) {
+                match filtered.get_mut(STATE_ATTR_IDENT) {
+                    None => {
+                        filtered.insert(STATE_ATTR_IDENT, vec![(idx, attr)]);
+                    }
+                    Some(v) => {
+                        v.push((idx, attr));
+                    }
+                }
+                // mark for removal
                 remove_idx[idx] = false;
             }
         }
 
+        // TODO consider the 0 case
+        let mut attr_type = Option::None;
+        match filtered.keys().len() {
+            1 => filtered.into_iter().for_each(|(k, v)| {
+                if v.len() > 1 {
+                    for (_, attr) in v {
+                        self.push_multiple_decl_error(attr, k);
+                    }
+                } else {
+                    attr_type = Option::Some(k);
+                }
+            }),
+            2 => filtered.into_iter().for_each(|(k, v)| {
+                if v.len() > 1 {
+                    for (_, attr) in v {
+                        self.push_multiple_decl_error(attr, k);
+                        self.push_multiple_attr_error(attr);
+                    }
+                } else {
+                    for (_, attr) in v {
+                        self.push_multiple_attr_error(attr);
+                    }
+                }
+            }),
+            _ => {
+                panic!("it was supposed to only be 1 or 2 keys")
+            }
+        }
+
+        // if errors were reported stop processing
+        if !self.errors.is_empty() {
+            return;
+        }
+
+        match attr_type.unwrap() {
+            // check for multiple automata definitions
+            AUTOMATA_ATTR_IDENT => {
+                match self.main_struct {
+                    Some(_) => self.push_multiple_automata_decl_error(it_struct),
+                    None => self.main_struct = Some(it_struct.clone()),
+                };
+                if let Err(e) = dbg!(add_state_type_param(it_struct)) {
+                    self.errors.push(e);
+                    return;
+                }
+            }
+            // add state
+            STATE_ATTR_IDENT => self.det_states.push(it_struct.clone()),
+            _ => panic!("unknown attribute passed the filters!!!"),
+        }
+
         remove_attrs(&mut it_struct.attrs, &remove_idx);
     }
+}
+
+fn add_state_type_param(automata_item: &mut ItemStruct) -> syn::Result<()> {
+    let struct_ident = &automata_item.ident;
+    let type_param_ident = format_ident!("{}State", struct_ident);
+
+    match &mut automata_item.fields {
+        syn::Fields::Named(named) => {
+            named.named.push(
+                Field::parse_named
+                    .parse2(quote!(state: #type_param_ident))
+                    .unwrap(),
+            );
+        }
+        syn::Fields::Unnamed(_) => {
+            return syn::Result::Err(Error::new_spanned(
+                automata_item,
+                "unnamed field structures are not supported",
+            ));
+        }
+        syn::Fields::Unit => {
+            automata_item.fields = Fields::Named(parse_quote! (state: #type_param_ident));
+        }
+    };
+
+    // add type parameter
+    automata_item
+        .generics
+        .params
+        .push(parse_quote!(#type_param_ident));
+    Ok(())
 }
 
 fn remove_attrs(attrs: &mut Vec<Attribute>, indexes: &Vec<bool>) {
