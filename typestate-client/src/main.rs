51c27702543833edc1eb48027ca5ad85f82f23f6
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-03-18 13:05:18 +0000

    Augment example

--- typestate-client/src/main.rs
+++ typestate-client/src/main.rs
@@ -1,10 +1,25 @@
+use m::*;
 use typestate::typestate;
+
+
+const N_CYCLES_MAINTENANCE: u64 = 1000;
+
+fn main() {
+    let red_light = TrafficLight::<Red>::turn_on();
+    let mut green_light = red_light.to_green();
+    if green_light.requires_maintenance() {
+        green_light.reset_cycles()
+    }
+    let yellow_light = green_light.to_yellow();
+    let red_light = yellow_light.to_red();
+    red_light.turn_off();
+}
 
 #[typestate]
 mod m {
     #[automata]
     pub struct TrafficLight {
-        cycles: u64,
+        pub cycles: u64,
     }
     #[state]
     pub struct Green;
@@ -13,21 +28,73 @@ mod m {
     #[state]
     pub struct Red;
     // #[transition]
-    trait Green {
+    pub trait Green {
         fn to_yellow(self) -> Yellow;
     }
-    trait Yellow {
+    pub trait Yellow {
         fn to_red(self) -> Red;
     }
-    trait Red {
+    pub trait Red {
         fn to_green(self) -> Green;
+        fn turn_on() -> Red;
+        fn turn_off(self);
     }
 }
 
-#[typestate]
-mod error_mod {
-    #[automata]
-    pub struct T;
+
+impl GreenState for TrafficLight<Green> {
+    fn to_yellow(self) -> TrafficLight<Yellow> {
+        println!("Green -> Yellow");
+        TrafficLight::<Yellow> {
+            cycles: self.cycles,
+            state: Yellow,
+        }
+    }
 }
 
-fn main() {}
+impl YellowState for TrafficLight<Yellow> {
+    fn to_red(self) -> TrafficLight<Red> {
+        println!("Yellow -> Red");
+        TrafficLight::<Red> {
+            // increment the cycle
+            cycles: self.cycles + 1,
+            state: Red,
+        }
+    }
+}
+
+impl RedState for TrafficLight<Red> {
+    fn to_green(self) -> TrafficLight<Green> {
+        println!("Red -> Green");
+        TrafficLight::<Green> {
+            cycles: self.cycles,
+            state: Green,
+        }
+    }
+
+    fn turn_on() -> TrafficLight<Red> {
+        println!("Turning on...");
+        TrafficLight::<Red> {
+            cycles: 0,
+            state: Red,
+        }
+    }
+
+    fn turn_off(self) {
+        println!("Turning off...");
+        // ... consume
+    }
+}
+
+impl<State> TrafficLight<State>
+where
+    State: TrafficLightState,
+{
+    fn requires_maintenance(&self) -> bool {
+        self.cycles > N_CYCLES_MAINTENANCE
+    }
+
+    fn reset_cycles(&mut self) {
+        self.cycles = 0;
+    }
+}
