718b5c56df53e825cb7f10e56d72e826d6cd91b2
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-03-31 16:05:11 +0100

    Add FA::new() and FA::default()

--- typestate-automata/src/automata.rs
+++ typestate-automata/src/automata.rs
@@ -21,3 +21,27 @@ where
     // Map of state indexes to map of transitions to state indexes.
     delta: HashMap<usize, HashMap<Transition, usize>>,
 }
+
+impl<State, Transition> FiniteAutomata<State, Transition>
+where
+    Transition: Eq + Hash,
+{
+    fn new() -> Self {
+        Self {
+            states: Vec::new(),
+            sigma: HashSet::new(),
+            initial_states: HashSet::new(),
+            final_states: HashSet::new(),
+            delta: HashMap::new(),
+        }
+    }
+}
+
+impl<State, Transition> Default for FiniteAutomata<State, Transition>
+where
+    Transition: Eq + Hash,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
