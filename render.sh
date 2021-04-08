2b0f4c7adf54c64b99f5489207fce16f2e932e2f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-04-08 12:08:28 +0100

    Add fix note to the render script

--- render.sh
+++ render.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# script is path sensitive
+# find way for it not to be
 DOT_GRAPHS="$(find . -iname "*.dot")"
 for graph in $DOT_GRAPHS; do
     graph_svg="$(echo "$graph" | sed "s/\.dot/.svg/")"
