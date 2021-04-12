94985735c11e574d11d5c57256c2bfeee9d64ee5
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-04-12 12:15:42 +0100

    Add support for more formats in the render script

--- render.sh
+++ render.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if [ "$#" -ne 1 ]; then
+    OUT_FORMAT="png"
+else
+    OUT_FORMAT="$1"
+fi
+
 # script is path sensitive
 # find way for it not to be
 DOT_GRAPHS="$(find . -iname "*.dot")"
 for graph in $DOT_GRAPHS; do
-    graph_svg="$(echo "$graph" | sed "s/\.dot/.svg/")"
-    dot "$graph" -Tsvg > "$graph_svg"
+    graph_out="$(echo "$graph" | sed "s/\.dot/.$OUT_FORMAT/")"
+    dot "$graph" -T"$OUT_FORMAT" > "$graph_out"
 done
