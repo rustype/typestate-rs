b2ec5ab2aea7cece5f7ebe8c7dac77c1b917a3aa
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-04-19 10:34:26 +0100

    Update trybuild test harness

--- typestate-macro/tests/macro_tests.rs
+++ typestate-macro/tests/macro_tests.rs
@@ -1,37 +1,6 @@
-macro_rules! fail {
-    ($tests:ident [$($head:ident),* $(,)?]) => {
-        $( $tests.compile_fail(format!("tests/{}.rs", stringify!($head))); )*
-    };
-}
-
-macro_rules! pass {
-    ($tests:ident [$($head:ident),* $(,)?]) => {
-        $( $tests.pass(format!("tests/{}.rs", stringify!($head))); )*
-    };
-}
-
 #[test]
 fn compile_fail() {
     let t = trybuild::TestCases::new();
-    fail!(t[
-        empty_module,
-        empty_automata,
-        duplicate_automata_attr,
-        duplicate_state_attr,
-        conflicting_automata_attr,
-        conflicting_state_attr,
-        missing_initial_final_states,
-        complex_unreachable_state,
-    ]);
-    pass!(t[
-        stateful_automata,
-    ]);
+    t.compile_fail("tests/fail/*.rs");
+    t.pass("tests/pass/*.rs");
 }
-
-// #[test]
-// fn compile_pass() {
-//     let t = trybuild::TestCases::new();
-//     pass!(t[
-//         stateful_automata,
-//     ]);
-// }
