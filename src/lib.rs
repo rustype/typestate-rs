d6a1742ca9061114bbcbb431450aabe980654af9
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-04-23 18:57:17 +0100

    Move det visitor to module

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use darling::FromMeta;
 use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::{format_ident, quote, ToTokens};
+use visitors::det;
 use std::{
     collections::{HashMap, HashSet},
     convert::TryFrom,
@@ -13,10 +14,7 @@ use syn::{parse::Parser, visit_mut::VisitMut, *};
 use typestate_automata::dot::*;
 use typestate_automata::{Dfa, Nfa};
 
-type Result<Ok, Err = Error> = ::core::result::Result<Ok, Err>;
 
-const AUTOMATA_ATTR_IDENT: &str = "automata";
-const STATE_ATTR_IDENT: &str = "state";
 
 #[proc_macro_attribute]
 pub fn typestate(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -52,23 +50,10 @@ pub fn typestate(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let mut state_machine_info = StateMachineInfo::new();
 
-    // start visitor
-    let mut state_visitor =
-        DeterministicStateVisitor::new(&mut state_machine_info, state_constructors_ident);
-    state_visitor.visit_item_mod_mut(&mut module);
-    // report state_visitor errors and return
-    bail_if_any!(state_visitor.errors);
+    let (sealed_trait, errors) = det::visit_states(&mut module, &mut state_machine_info, state_constructors_ident);
+    bail_if_any!(errors);
 
-    let mut constructors = state_visitor.constructors;
-    if let Some((_, v)) = &mut module.content {
-        v.append(&mut constructors);
-    }
-
-    let sealed_trait = state_visitor.sealed_trait;
-    if sealed_trait.trait_ident.is_none() {
-        return TypestateError::MissingAutomata.into_compile_error().into();
-    }
-
+    // Visit non-deterministic transitions
     bail_if_any!(visitors::non_det::visit_non_deterministic(
         &mut module,
         &mut state_machine_info
@@ -148,9 +133,10 @@ pub fn typestate(args: TokenStream, input: TokenStream) -> TokenStream {
     // appending new code should happen after all other code is processed
     // since this adds the sealed pattern traits and those aren't valid states
     // if this is done before visiting transitions the generated code is flagged as invalid transitions
+    // TODO solution -> annotated generated code with #[typestate::do_not_parse]
     match &mut module.content {
         Some((_, v)) => {
-            v.append(&mut sealed_trait.into());
+            v.append(&mut sealed_trait.unwrap().into()); // HACK unwrap is safe because otherwise errors would've bailed
         }
         None => {}
     }
@@ -288,41 +274,6 @@ impl IntoCompileError for Vec<Error> {
                 .to_compile_error();
         }
         TokenStream2::new()
-    }
-}
-
-#[derive(Debug, PartialEq)]
-enum TypestateAttr {
-    Automata,
-    State,
-}
-
-impl TryFrom<&Ident> for TypestateAttr {
-    // TODO take care of this error type
-    type Error = ();
-
-    fn try_from(ident: &Ident) -> Result<Self, Self::Error> {
-        if ident == AUTOMATA_ATTR_IDENT {
-            Ok(Self::Automata)
-        } else if ident == STATE_ATTR_IDENT {
-            Ok(Self::State)
-        } else {
-            Err(())
-        }
-    }
-}
-
-impl TryFrom<&Path> for TypestateAttr {
-    type Error = ();
-
-    fn try_from(path: &Path) -> Result<Self, Self::Error> {
-        if path.is_ident(AUTOMATA_ATTR_IDENT) {
-            Ok(Self::Automata)
-        } else if path.is_ident(STATE_ATTR_IDENT) {
-            Ok(Self::State)
-        } else {
-            Err(())
-        }
     }
 }
 
@@ -535,240 +486,6 @@ impl From<StateMachineInfo> for FiniteAutomata<Ident, Ident> {
             }
             FiniteAutomata::NonDeterministic(name, nfa)
         }
-    }
-}
-
-#[derive(Default)]
-struct SealedPattern {
-    /// Ident for the sealed pattern public trait
-    trait_ident: Option<Ident>, // late init
-    /// Idents for the sealed elements.
-    state_idents: Vec<Ident>,
-}
-
-// TODO rework this as an ExpandX trait
-impl From<SealedPattern> for Vec<Item> {
-    /// Convert the SealedTrait into a vector of Item.
-    /// This enables the addition of new items to the main module.
-    fn from(sealed_pattern: SealedPattern) -> Self {
-        let trait_ident = sealed_pattern.trait_ident.expect("missing `.trait_ident`");
-        let private_mod_ident = format_ident!("__private");
-        // or `Private` or `Sealed` or `format_ident!("{}Sealed", …)`
-        // take into account that `trait_ident` may have already been used
-        let private_mod_trait = &trait_ident;
-
-        let states = &sealed_pattern.state_idents;
-        let mut ret = vec![
-            // Sealed trait
-            ::syn::parse_quote! {
-                /* private */ mod #private_mod_ident {
-                    pub trait #private_mod_trait {}
-                }
-            },
-            // State trait
-            ::syn::parse_quote! {
-                pub trait #trait_ident: #private_mod_ident::#private_mod_trait {}
-            },
-            // Blanket impl of state trait from sealed implementors
-            // This frees us from having to provide concrete impls for each type.
-            ::syn::parse_quote! {
-                impl<__T : ?::core::marker::Sized> #trait_ident
-                    for __T
-                where
-                    __T : #private_mod_ident::#private_mod_trait,
-                {}
-            },
-        ];
-
-        // Sealed trait impls
-        ret.extend(states.iter().map(|each_state| {
-            ::syn::parse_quote! {
-                impl #private_mod_ident::#private_mod_trait for #each_state {}
-            }
-        }));
-
-        ret
-    }
-}
-
-trait ExpandStateConstructors {
-    fn expand_state_constructors(&mut self, constructor_ident: &Ident, item_struct: &ItemStruct);
-}
-
-impl ExpandStateConstructors for Vec<Item> {
-    fn expand_state_constructors(&mut self, constructor_ident: &Ident, item_struct: &ItemStruct) {
-        if let Fields::Named(named) = &item_struct.fields {
-            let struct_ident = &item_struct.ident;
-            let field_ident = named.named.iter().map(|field| &field.ident);
-            let field_ident2 = named.named.iter().map(|field| &field.ident); // HACK
-            let field_ty = named.named.iter().map(|field| &field.ty);
-            let tokens = quote! {
-                impl #struct_ident {
-                    pub fn #constructor_ident(#(#field_ident: #field_ty,)*) -> Self {
-                        Self {
-                            #(#field_ident2,)*
-                        }
-                    }
-                }
-            };
-            self.push(parse_quote!(#tokens));
-        }
-    }
-}
-
-struct DeterministicStateVisitor<'sm> {
-    /// State machine required information
-    state_machine_info: &'sm mut StateMachineInfo,
-    /// Sealed trait information
-    sealed_trait: SealedPattern,
-    /// Default constructors
-    constructors: Vec<Item>,
-    /// Default constructor ident
-    constructor_ident: Option<Ident>,
-    /// Errors found during expansion
-    errors: Vec<Error>,
-}
-
-impl<'sm> DeterministicStateVisitor<'sm> {
-    fn new(
-        state_machine_info: &'sm mut StateMachineInfo,
-        constructor_ident: Option<Ident>,
-    ) -> Self {
-        Self {
-            state_machine_info,
-            sealed_trait: SealedPattern::default(),
-            constructors: vec![],
-            constructor_ident,
-            errors: vec![],
-        }
-    }
-
-    /// Add `multiple attributes` error to the error vector.
-    fn push_multiple_attr_error(&mut self, attr: &Attribute) {
-        self.errors
-            .push(TypestateError::ConflictingAttributes(attr.clone()).into());
-    }
-
-    /// Add `duplicate attribute` error to the error vector.
-    fn push_multiple_decl_error(&mut self, attr: &Attribute) {
-        self.errors
-            .push(TypestateError::DuplicateAttributes(attr.clone()).into());
-    }
-
-    /// Add `multiple automata` error to the error vector.
-    fn push_multiple_automata_decl_error(&mut self, it: &ItemStruct) {
-        self.errors
-            .push(TypestateError::AutomataRedefinition(it.clone()).into());
-    }
-}
-
-#[derive(PartialEq)]
-enum Attr {
-    Retain,
-    Discard,
-}
-
-impl<'sm> VisitMut for DeterministicStateVisitor<'sm> {
-    fn visit_item_struct_mut(&mut self, it_struct: &mut ItemStruct) {
-        let attributes = &mut it_struct.attrs;
-        let mut main_attr = None;
-        attributes.retain(|attr| {
-            Attr::Retain == {
-                let ts_attr = TypestateAttr::try_from(&attr.path);
-                match ts_attr {
-                    Ok(inner_ts_attr) => {
-                        match main_attr {
-                            Some(ref prev_attr) => {
-                                if *prev_attr == inner_ts_attr {
-                                    self.push_multiple_decl_error(attr);
-                                } else {
-                                    self.push_multiple_attr_error(attr);
-                                }
-                            }
-                            ref mut at_none @ None => {
-                                // only if it wasnt previously assigned we can assign a new value
-                                *at_none = Some(inner_ts_attr)
-                            }
-                        }
-                        Attr::Discard
-                    }
-                    Err(()) => Attr::Retain,
-                }
-            }
-        });
-
-        // if errors were reported stop processing
-        if !self.errors.is_empty() {
-            return;
-        }
-
-        match main_attr {
-            Some(TypestateAttr::Automata) => {
-                // check for multiple automata definitions
-                match self.state_machine_info.main_struct {
-                    Some(_) => {
-                        self.push_multiple_automata_decl_error(it_struct);
-                        return;
-                    }
-                    None => self.state_machine_info.main_struct = Some(it_struct.clone()),
-                };
-                match it_struct.expand_state_type_parameter() {
-                    Ok(bound_ident) => match self.sealed_trait.trait_ident {
-                        Some(_) => unreachable!("this should have been checked previously"),
-                        None => self.sealed_trait.trait_ident = Some(bound_ident),
-                    },
-                    Err(e) => {
-                        self.errors.push(e);
-                    }
-                }
-            }
-            Some(TypestateAttr::State) => {
-                self.state_machine_info.add_state(it_struct.clone().into());
-                self.sealed_trait.state_idents.push(it_struct.ident.clone());
-                if let Some(ident) = &self.constructor_ident {
-                    self.constructors
-                        .expand_state_constructors(ident, it_struct);
-                }
-            }
-            None => {
-                // empty attribute list
-            }
-        }
-    }
-}
-
-trait ExpandState {
-    /// Expand the state type parameter in a structure or other kind of item.
-    fn expand_state_type_parameter(&mut self) -> syn::Result<Ident>;
-}
-
-impl ExpandState for ItemStruct {
-    fn expand_state_type_parameter(&mut self) -> syn::Result<Ident> {
-        // TODO make the suffix custom
-        let type_param_ident = format_ident!("{}State", self.ident);
-        self.generics
-            .params
-            .push(::syn::parse_quote!(State: #type_param_ident));
-
-        let field_to_add = quote!(
-            pub state: State
-        );
-
-        match &mut self.fields {
-            syn::Fields::Named(named) => {
-                named
-                    .named
-                    .push(Field::parse_named.parse2(field_to_add).unwrap());
-            }
-            syn::Fields::Unnamed(_) => {
-                return syn::Result::Err(TypestateError::UnsupportedStruct(self.clone()).into());
-            }
-            syn::Fields::Unit => {
-                self.fields = Fields::Named(::syn::parse_quote!({ #field_to_add }));
-            }
-        };
-
-        Ok(type_param_ident)
     }
 }
 
---
+++ src/visitors/det.rs
@@ -0,0 +1,306 @@
+use std::convert::TryFrom;
+
+use crate::{StateMachineInfo, TypestateError};
+
+use parse::Parser;
+use syn::{visit_mut::VisitMut, *};
+
+const AUTOMATA_ATTR_IDENT: &str = "automata";
+const STATE_ATTR_IDENT: &str = "state";
+
+type Result<Ok, Err = Error> = ::core::result::Result<Ok, Err>;
+
+// HACK return type
+pub(crate) fn visit_states(
+    module: &mut ItemMod,
+    state_machine_info: &mut StateMachineInfo,
+    constructor_ident: Option<Ident>,
+) -> (Option<SealedPattern>, Vec<Error>) {
+    // start visitor
+    let mut state_visitor = DeterministicStateVisitor::new(state_machine_info, constructor_ident);
+    state_visitor.visit_item_mod_mut(module);
+    // report state_visitor errors and return
+    if !state_visitor.errors.is_empty() {
+        return (None, state_visitor.errors);
+    }
+
+    let mut constructors = state_visitor.constructors;
+    if let Some((_, v)) = &mut module.content {
+        v.append(&mut constructors);
+    }
+
+    let sealed_trait = state_visitor.sealed_trait;
+    if sealed_trait.trait_ident.is_none() {
+        return (None, vec![TypestateError::MissingAutomata.into()]);
+    }
+    (Some(sealed_trait), vec![])
+}
+
+struct DeterministicStateVisitor<'sm> {
+    /// State machine required information
+    state_machine_info: &'sm mut StateMachineInfo,
+    /// Sealed trait information
+    sealed_trait: SealedPattern,
+    /// Default constructors
+    constructors: Vec<Item>,
+    /// Default constructor ident
+    constructor_ident: Option<Ident>,
+    /// Errors found during expansion
+    errors: Vec<Error>,
+}
+
+impl<'sm> DeterministicStateVisitor<'sm> {
+    fn new(
+        state_machine_info: &'sm mut StateMachineInfo,
+        constructor_ident: Option<Ident>,
+    ) -> Self {
+        Self {
+            state_machine_info,
+            sealed_trait: SealedPattern::default(),
+            constructors: vec![],
+            constructor_ident,
+            errors: vec![],
+        }
+    }
+
+    /// Add `multiple attributes` error to the error vector.
+    fn push_multiple_attr_error(&mut self, attr: &Attribute) {
+        self.errors
+            .push(TypestateError::ConflictingAttributes(attr.clone()).into());
+    }
+
+    /// Add `duplicate attribute` error to the error vector.
+    fn push_multiple_decl_error(&mut self, attr: &Attribute) {
+        self.errors
+            .push(TypestateError::DuplicateAttributes(attr.clone()).into());
+    }
+
+    /// Add `multiple automata` error to the error vector.
+    fn push_multiple_automata_decl_error(&mut self, it: &ItemStruct) {
+        self.errors
+            .push(TypestateError::AutomataRedefinition(it.clone()).into());
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct SealedPattern {
+    /// Ident for the sealed pattern public trait
+    trait_ident: Option<Ident>, // late init
+    /// Idents for the sealed elements.
+    state_idents: Vec<Ident>,
+}
+
+// TODO rework this as an ExpandX trait
+impl From<SealedPattern> for Vec<Item> {
+    /// Convert the SealedTrait into a vector of Item.
+    /// This enables the addition of new items to the main module.
+    fn from(sealed_pattern: SealedPattern) -> Self {
+        let trait_ident = sealed_pattern.trait_ident.expect("missing `.trait_ident`");
+        let private_mod_ident = ::quote::format_ident!("__private");
+        // or `Private` or `Sealed` or `format_ident!("{}Sealed", …)`
+        // take into account that `trait_ident` may have already been used
+        let private_mod_trait = &trait_ident;
+
+        let states = &sealed_pattern.state_idents;
+        let mut ret = vec![
+            // Sealed trait
+            ::syn::parse_quote! {
+                /* private */ mod #private_mod_ident {
+                    pub trait #private_mod_trait {}
+                }
+            },
+            // State trait
+            ::syn::parse_quote! {
+                pub trait #trait_ident: #private_mod_ident::#private_mod_trait {}
+            },
+            // Blanket impl of state trait from sealed implementors
+            // This frees us from having to provide concrete impls for each type.
+            ::syn::parse_quote! {
+                impl<__T : ?::core::marker::Sized> #trait_ident
+                    for __T
+                where
+                    __T : #private_mod_ident::#private_mod_trait,
+                {}
+            },
+        ];
+
+        // Sealed trait impls
+        ret.extend(states.iter().map(|each_state| {
+            ::syn::parse_quote! {
+                impl #private_mod_ident::#private_mod_trait for #each_state {}
+            }
+        }));
+
+        ret
+    }
+}
+
+impl<'sm> VisitMut for DeterministicStateVisitor<'sm> {
+    fn visit_item_struct_mut(&mut self, it_struct: &mut ItemStruct) {
+        let attributes = &mut it_struct.attrs;
+        let mut main_attr = None;
+        attributes.retain(|attr| {
+            Attr::Retain == {
+                let ts_attr = TypestateAttr::try_from(&attr.path);
+                match ts_attr {
+                    Ok(inner_ts_attr) => {
+                        match main_attr {
+                            Some(ref prev_attr) => {
+                                if *prev_attr == inner_ts_attr {
+                                    self.push_multiple_decl_error(attr);
+                                } else {
+                                    self.push_multiple_attr_error(attr);
+                                }
+                            }
+                            ref mut at_none @ None => {
+                                // only if it wasnt previously assigned we can assign a new value
+                                *at_none = Some(inner_ts_attr)
+                            }
+                        }
+                        Attr::Discard
+                    }
+                    Err(()) => Attr::Retain,
+                }
+            }
+        });
+
+        // if errors were reported stop processing
+        if !self.errors.is_empty() {
+            return;
+        }
+
+        match main_attr {
+            Some(TypestateAttr::Automata) => {
+                // check for multiple automata definitions
+                match self.state_machine_info.main_struct {
+                    Some(_) => {
+                        self.push_multiple_automata_decl_error(it_struct);
+                        return;
+                    }
+                    None => self.state_machine_info.main_struct = Some(it_struct.clone()),
+                };
+                match it_struct.expand_state_type_parameter() {
+                    Ok(bound_ident) => match self.sealed_trait.trait_ident {
+                        Some(_) => unreachable!("this should have been checked previously"),
+                        None => self.sealed_trait.trait_ident = Some(bound_ident),
+                    },
+                    Err(e) => {
+                        self.errors.push(e);
+                    }
+                }
+            }
+            Some(TypestateAttr::State) => {
+                self.state_machine_info.add_state(it_struct.clone().into());
+                self.sealed_trait.state_idents.push(it_struct.ident.clone());
+                if let Some(ident) = &self.constructor_ident {
+                    self.constructors
+                        .expand_state_constructors(ident, it_struct);
+                }
+            }
+            None => {
+                // empty attribute list
+            }
+        }
+    }
+}
+
+trait ExpandStateConstructors {
+    fn expand_state_constructors(&mut self, constructor_ident: &Ident, item_struct: &ItemStruct);
+}
+
+impl ExpandStateConstructors for Vec<Item> {
+    fn expand_state_constructors(&mut self, constructor_ident: &Ident, item_struct: &ItemStruct) {
+        if let Fields::Named(named) = &item_struct.fields {
+            let struct_ident = &item_struct.ident;
+            let field_ident = named.named.iter().map(|field| &field.ident);
+            let field_ident2 = named.named.iter().map(|field| &field.ident); // HACK
+            let field_ty = named.named.iter().map(|field| &field.ty);
+            let tokens = ::quote::quote! {
+                impl #struct_ident {
+                    pub fn #constructor_ident(#(#field_ident: #field_ty,)*) -> Self {
+                        Self {
+                            #(#field_ident2,)*
+                        }
+                    }
+                }
+            };
+            self.push(parse_quote!(#tokens));
+        }
+    }
+}
+
+trait ExpandState {
+    /// Expand the state type parameter in a structure or other kind of item.
+    fn expand_state_type_parameter(&mut self) -> syn::Result<Ident>;
+}
+
+impl ExpandState for ItemStruct {
+    fn expand_state_type_parameter(&mut self) -> syn::Result<Ident> {
+        // TODO make the suffix custom
+        let type_param_ident = ::quote::format_ident!("{}State", self.ident);
+        self.generics
+            .params
+            .push(::syn::parse_quote!(State: #type_param_ident));
+
+        let field_to_add = ::quote::quote!(
+            pub state: State
+        );
+
+        match &mut self.fields {
+            syn::Fields::Named(named) => {
+                named
+                    .named
+                    .push(Field::parse_named.parse2(field_to_add).unwrap());
+            }
+            syn::Fields::Unnamed(_) => {
+                return syn::Result::Err(TypestateError::UnsupportedStruct(self.clone()).into());
+            }
+            syn::Fields::Unit => {
+                self.fields = Fields::Named(::syn::parse_quote!({ #field_to_add }));
+            }
+        };
+
+        Ok(type_param_ident)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum TypestateAttr {
+    Automata,
+    State,
+}
+
+impl TryFrom<&Ident> for TypestateAttr {
+    // TODO take care of this error type
+    type Error = ();
+
+    fn try_from(ident: &Ident) -> Result<Self, Self::Error> {
+        if ident == AUTOMATA_ATTR_IDENT {
+            Ok(Self::Automata)
+        } else if ident == STATE_ATTR_IDENT {
+            Ok(Self::State)
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl TryFrom<&Path> for TypestateAttr {
+    type Error = ();
+
+    fn try_from(path: &Path) -> Result<Self, Self::Error> {
+        if path.is_ident(AUTOMATA_ATTR_IDENT) {
+            Ok(Self::Automata)
+        } else if path.is_ident(STATE_ATTR_IDENT) {
+            Ok(Self::State)
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[derive(PartialEq)]
+enum Attr {
+    Retain,
+    Discard,
+}
