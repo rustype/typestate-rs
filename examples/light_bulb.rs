b23449fceca4c09d9291cdccfb974f1488776961
José Duarte <jmg.duarte@campus.fct.unl.pt!>
2021-06-05 18:24:08 +0100

    chore: implement the light bulb example

--- examples/light_bulb.rs
+++ examples/light_bulb.rs
@@ -1,21 +1,45 @@
+use light_bulb::*;
 use typestate::typestate;
 
 #[typestate]
 mod light_bulb {
     #[automaton]
-    struct LightBulb;
+    pub struct LightBulb;
 
-    #[state] struct Off;
-    trait Off {
+    #[state]
+    pub struct Off;
+    pub trait Off {
         fn screw() -> Off;
         fn unscrew(self);
         fn turn_on(self) -> On; // Off => On transition
     }
 
-    #[state] struct On;
-    trait On {
+    #[state]
+    pub struct On;
+    pub trait On {
         fn turn_off(self) -> Off;
     }
 }
 
-fn main() {}
+impl OffState for LightBulb<Off> {
+    fn screw() -> LightBulb<Off> {
+        Self { state: Off }
+    }
+    fn unscrew(self) {}
+    fn turn_on(self) -> LightBulb<On> {
+        LightBulb::<On> { state: On }
+    }
+}
+
+impl OnState for LightBulb<On> {
+    fn turn_off(self) -> LightBulb<Off> {
+        LightBulb::<Off> { state: Off }
+    }
+}
+
+fn main() {
+    let bulb = LightBulb::<Off>::screw();
+    let bulb = bulb.turn_on();
+    let bulb = bulb.turn_off();
+    let _ = bulb.turn_off();
+}
