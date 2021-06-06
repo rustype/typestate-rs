2c2b8d7f81804abde5f0f787a7dc5f0428f531ae
José Duarte <jmg.duarte@campus.fct.unl.pt!>
2021-06-06 16:18:27 +0100

    feat: implement variant visitor

--- typestate-proc-macro/src/visitors/decision.rs
+++ typestate-proc-macro/src/visitors/decision.rs
@@ -1,9 +1,7 @@
-use proc_macro2::TokenStream as TokenStream2;
-use syn::{
-    parse_macro_input, visit_mut::VisitMut, Error, Fields, Ident, ItemEnum, ItemMod, Variant,
-};
+use darling::FromMeta;
+use syn::{visit_mut::VisitMut, Error, Fields, Ident, ItemEnum, ItemMod, Variant};
 
-use crate::{StateMachineInfo, TypestateError};
+use crate::{intermediate_graph::TransitionMetadata, StateMachineInfo, TypestateError};
 
 macro_rules! bail_if_any {
     ( $errors:expr ) => {
@@ -40,6 +38,94 @@ impl<'sm> DecisionVisitor<'sm> {
             errors: vec![],
         }
     }
+}
+
+impl<'sm> VisitMut for DecisionVisitor<'sm> {
+    fn visit_item_enum_mut(&mut self, i: &mut ItemEnum) {
+        let mut v = DecisionVariantVisitor::new(self.state_machine_info);
+        for variant in &mut i.variants {
+            v.visit_variant_mut(variant);
+        }
+        if v.errors.is_empty() {
+            let enum_ident = i.ident.clone();
+            let destination_idents: Vec<Ident> =
+                i.variants.iter().cloned().map(|v| v.ident).collect();
+            // TODO: implement https://github.com/rustype/typestate-rs/issues/3
+            // NOTE: this could be `Option<Ident>`
+
+            self.state_machine_info
+                .intermediate_automaton
+                .add_transition(
+                    enum_ident.clone().into(),
+                    enum_ident.clone().into(),
+                    destination_idents.into(),
+                );
+
+            // self.state_machine_info.add_state(i.clone().into());
+            self.state_machine_info
+                .non_det_transitions
+                .insert(enum_ident, i.clone());
+        } else {
+            self.errors.extend(v.errors);
+        }
+    }
+}
+
+struct DecisionVariantVisitor<'a> {
+    info: &'a mut StateMachineInfo,
+    metadata: Option<TransitionMetadata>,
+    errors: Vec<Error>,
+}
+
+impl<'sm> DecisionVariantVisitor<'sm> {
+    fn new(info: &'sm mut StateMachineInfo) -> Self {
+        Self {
+            info,
+            metadata: None,
+            errors: vec![],
+        }
+    }
+
+    fn parse_metadata(&mut self, variant: &mut Variant) {
+        variant.attrs.retain(|attr| {
+            if attr.path.is_ident("metadata") {
+                match attr.parse_meta() {
+                    Ok(meta) => match TransitionMetadata::from_meta(&meta) {
+                        Ok(metadata) => self.metadata = Some(metadata),
+                        // TODO
+                        Err(_err) => todo!("make enum for the errors or something"),
+                    },
+                    Err(err) => self.errors.push(err),
+                }
+                false
+            } else {
+                true
+            }
+        })
+    }
+
+    fn parse_variant(&mut self, variant: &mut Variant) {
+        // check if the variant is a valid one
+        // i.e. unit-style variant
+        if let Fields::Unit = &variant.fields {
+            let ident = &variant.ident;
+            // check if the current ident is a valid state or another decision node
+            if self.info.non_det_transitions.contains_key(ident) {
+                self.push_unsupported_state_error(ident);
+            } else if self.info.det_states.contains_key(ident) {
+                let automata_ident = self.info.get_automaton_ident();
+                variant.fields = Fields::Unnamed(::syn::parse_quote!(
+                    /* Variant */ (
+                        #automata_ident<#ident>
+                    )
+                ));
+            } else {
+                self.push_undeclared_variant_error(ident);
+            }
+        } else {
+            self.push_unsupported_variant_error(variant);
+        }
+    }
 
     /// Add `undeclared state` error to the error vector.
     fn push_undeclared_variant_error(&mut self, ident: &Ident) {
@@ -60,65 +146,9 @@ impl<'sm> DecisionVisitor<'sm> {
     }
 }
 
-impl<'sm> VisitMut for DecisionVisitor<'sm> {
-    fn visit_item_enum_mut(&mut self, i: &mut ItemEnum) {
-        for variant in &mut i.variants {
-            // let attrs = variant.attrs;
-
-            // let mut transition_metadata = None;
-            // for attr in attrs {
-            //     if attr.path.is_ident("metadata") {
-            //         let attr_tokens = attr.into();
-            //         let metadata: TransitionMetadata = parse_macro_input!(attr_tokens);
-            //         transition_metadata = metadata.into();
-            //     }
-            // }
-            // eprintln!("{:#?}", transition_metadata);
-
-            // check if the variant is a valid one
-            // i.e. unit-style variant
-            if let Fields::Unit = &variant.fields {
-                let ident = &variant.ident;
-                // check if the current ident is a valid state or another decision node
-                if self
-                    .state_machine_info
-                    .non_det_transitions
-                    .contains_key(ident)
-                {
-                    self.push_unsupported_state_error(ident);
-                } else if self.state_machine_info.det_states.contains_key(ident) {
-                    let automata_ident = self.state_machine_info.get_automaton_ident();
-                    variant.fields = Fields::Unnamed(::syn::parse_quote!(
-                        /* Variant */ (
-                            #automata_ident<#ident>
-                        )
-                    ));
-                } else {
-                    self.push_undeclared_variant_error(ident);
-                }
-            } else {
-                self.push_unsupported_variant_error(variant);
-            }
-        }
-        if self.errors.is_empty() {
-            let enum_ident = i.ident.clone();
-            let destination_idents: Vec<Ident> =
-                i.variants.iter().cloned().map(|v| v.ident).collect();
-            // TODO: implement https://github.com/rustype/typestate-rs/issues/3
-            // NOTE: this could be `Option<Ident>`
-
-            self.state_machine_info
-                .intermediate_automaton
-                .add_transition(
-                    enum_ident.clone().into(),
-                    enum_ident.clone().into(),
-                    destination_idents.into(),
-                );
-
-            // self.state_machine_info.add_state(i.clone().into());
-            self.state_machine_info
-                .non_det_transitions
-                .insert(enum_ident, i.clone());
-        }
+impl VisitMut for DecisionVariantVisitor<'_> {
+    fn visit_variant_mut(&mut self, variant: &mut Variant) {
+        self.parse_metadata(variant);
+        self.parse_variant(variant);
     }
 }
